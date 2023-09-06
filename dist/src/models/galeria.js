3df2d13786bd1a5b4d64afb6d8c5f956988d55f9
Lucas Souza <=>
2023-09-06 08:22:16 -0300

    remove dist folder

--- dist/src/models/galeria.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Galeria = void 0;
-const core_1 = require("./core");
-class Galeria extends core_1.Core {
-}
-exports.Galeria = Galeria;
--- dist/src/program.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const startUp_1 = require("./startUp");
-const port = '5000';
-startUp_1.startUp.app.listen(port, () => {
-    console.info(`server running in  localhost:${port}`);
-});
